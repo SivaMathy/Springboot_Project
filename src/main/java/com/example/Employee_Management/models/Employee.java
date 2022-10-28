27555ba7bd25fcc0ffc5a93cfb8cc69c0432c8d8
SivaMathy <mathyvathanasivalingam@gmail.com>
2022-10-28 15:57:18 +0530

    Image uploading part

--- src/main/java/com/example/Employee_Management/models/Employee.java
+++ src/main/java/com/example/Employee_Management/models/Employee.java
@@ -5,10 +5,12 @@ import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
@@ -26,55 +28,50 @@ public class Employee {
     @Size(min = 2, message = "Employee ID should have at least 2 characters")
     @NonNull
     private String eid;
+
     @Size(min = 2, message = "Employee Name should have at least 2 characters")
     @NotBlank(message = "Name is mandatory")
     private String ename;
+
     @NotBlank(message = "Address is mandatory")
     private String address;
+
     @NotBlank(message = "Basic Salary is mandatory")
     private String basicsalry;
+
     @NotBlank(message = "Designation is mandatory")
     private String designation;
+
     @NotBlank(message = "Email is mandatory")
     @Email
     private String email;
-    @NotBlank(message = "Status is mandatory")
+
     private String status;
+
+
     @NotBlank(message = "Department is mandatory")
     private String department;
+
     @NotBlank(message = "Bank Name  is mandatory")
-    private String name_of_bank;
+    private String nameOfBank;
+
     @NotBlank(message = "Bank branch is mandatory")
     private String branch;
-    @NotBlank(message = "Account Number is mandatory")
+
     @NotBlank(message = "User Role is mandatory")
     private String userRole;
-    @Size(min = 1,max = 12,message = "Account Number should have at least 2 numbers and maximum 12 number ")
-    private String account_no;
 
-    private String password = String.valueOf(passwordGenerator());
+    @Size(min = 1, max = 12, message = "Account Number should have at least 2 numbers and maximum 12 number ")
+    private String accountNo;
 
+    private String password;
 
-
-
-    public char[] passwordGenerator() {
-        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        String lower = "abcdefghijklmnopqrstuvwxyz";
-        String num = "1234567890";
-        String charcter = "@#$%^&*+_";
-
-        String combination = upper + lower + num + charcter;
-        int len = 8;
-        char[] password = new char[len];
-        Random r = new Random();
-        for (int i = 0; i < len; i++) {
-            password[i] = combination.charAt(r.nextInt(combination.length()));
-
-        }
-
-        return password;
+    private Date joinedDate;
 
 
-    }
+    @NotBlank(message = "NIC number is mandatory")
+    private String nic;
+
+    private MultipartFile nicImage;
 
 }
